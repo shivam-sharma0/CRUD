3202ad3cb3a8a0fa2e20b297eae20b79af285d6d
Shivam Sharma <124781117+shivam-sharma0@users.noreply.github.com>
2023-04-29 14:32:18 +0530

    Add files via upload

---
+++ api_making/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public int IsActive { get; set; }
+
+    }
+}
---
+++ api_making/Models/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
